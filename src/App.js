f4e07c7c90fe3c0be1b66c7d94961aec2f1c2a18
Chase Beard <chasebeard17@gmail.com>
2019-05-31 14:30:00 -0400

    client folder

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import logo from "./logo.svg";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*
+class App extends Component {
+  state = {users: []}
+
+  componentDidMount() {
+    fetch('/posts')
+      .then(res => res.json())
+      .then(users => this.setState({ users }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Users</h1>
+        {this.state.users.map(user =>
+          <div key={user.id}>{user.username}</div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
+*/
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {posts: ""};
+  }
+
+
+  callAPI() {
+    fetch('http://localhost:3001/posts')
+      .then(res => res.text())
+      .then(posts => this.setState({posts}))
+      .catch(err => err);
+  }
+
+  componentDidMount() {
+      this.callAPI();
+  }
+
+
+
+  render() {
+      return (
+          <div className="App">
+              <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <h1 className="App-title">Welcome to React</h1>
+              </header>
+              <p className="App-intro">{this.state.posts}</p>
+          </div>
+      );
+  }
 }
 
 export default App;
